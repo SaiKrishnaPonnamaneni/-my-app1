a31cde3109c98f14a3d7d6c175b457c3a3474d86
SaiKrishnaPonnamaneni <psai8307@gmail.com>
2024-11-27 09:57:24 +0530

    create user validations

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -11,10 +11,10 @@ export class CreateUserComponent {
   public userForm:FormGroup=new FormGroup({
     name: new FormControl("",[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
     age: new FormControl("",[Validators.required,Validators.min(18),Validators.max(60)]),
-    email: new FormControl(),
-    phone: new FormControl(),
+    email: new FormControl("",[Validators.required,Validators.email]),
+    phone: new FormControl("",[Validators.required,Validators.min(100000000),Validators.max(9999999999)]),
     address:new FormGroup({
-      city:new FormControl(),
+      city:new FormControl('',[Validators.required]),
       pin:new FormControl()
     }),
 
@@ -31,7 +31,7 @@ export class CreateUserComponent {
   addCard(){
     this.cardsFormArray.push(
       new FormGroup({
-        number:new FormControl(),
+        number:new FormControl('',[Validators.required]),
         expiry:new FormControl(),
         cvv: new FormControl(),
       })
